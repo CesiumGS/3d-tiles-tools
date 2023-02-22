50f61be001b5972461a57e5f6262708f0c6bb018
Marco Hutter <javagl@javagl.de>
2023-02-22 19:07:58 +0100

    Trying to revive 'optimize' from gltf-pipeline

--- src/contentOperations/GtlfUtilities.ts
+++ src/contentOperations/GtlfUtilities.ts
@@ -1,4 +1,5 @@
 import GltfPipeline from "gltf-pipeline";
+import { GltfPipelineLegacy } from "./GltfPipelineLegacy";
 
 /**
  * Internal utility methods related to glTF/GLB data.
@@ -20,85 +21,29 @@ export class GltfUtilities {
     return result.glb;
   }
 
-  //==========================================================================
-  // Ported From https://github.com/CesiumGS/3d-tiles-tools/blob/37cffa759af301e44641bc89b4db5d06a3bdb800/lib/optimizeGlb.js#L58
   /**
-   * Given an input buffer containing a binary glTF asset, optimize it using gltf-pipeline with the provided options
+   * Given an input buffer containing a binary glTF asset, optimize it
+   * using gltf-pipeline with the provided options.
+   *
+   * This method also performs a few updates of certain legacy
+   * features that are specific for the GLB data that is part
+   * of I3DM and B3DM. Details are not specified here.
    *
    * @param glbBuffer The buffer containing the binary glTF.
    * @param options Options specifying custom gltf-pipeline behavior.
    * @returns A promise that resolves to the optimized binary glTF.
    */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   static async optimizeGlb(glbBuffer: Buffer, options: any): Promise<Buffer> {
-    console.error("OptimizeGlb is not implemented yet");
-    return glbBuffer;
-
-    // TODO Update this. Whatever parseBinaryGltf is now...
-    /*
-    options = defaultValue(options, {});
-    let rtcPosition;
-    const gltf = GltfPipeline.parseBinaryGltf(glbBuffer);
-    const extensions = gltf.extensions;
-    if (extensions) {
-      // If it is used, extract the CesiumRTC extension and add it back after processing
-      const cesiumRTC = extensions.CESIUM_RTC;
-      if (cesiumRTC) {
-        rtcPosition = Cartesian3.unpack(cesiumRTC.center);
-      }
+    options = options ?? {};
+    if (!options.customStages) {
+      options.customStages = [];
     }
-    GltfUtilities.fixBatchIdSemantic(gltf);
-    await GltfPipeline.loadGltfUris(gltf, options);
-    const processedGltf = await GltfPipeline.processJSONWithExtras(
-      gltf,
-      options
-    );
-    if (rtcPosition) {
-      GltfPipeline.addCesiumRTC(processedGltf, {
-        position: rtcPosition,
-      });
-    }
-    const embed = defaultValue(options.embed, true);
-    const embedImage = defaultValue(options.embedImage, true);
-    const result = GltfPipeline.getBinaryGltf(
-      processedGltf,
-      embed,
-      embedImage
-    ).glb;
-    return result;
-    */
+    const customStage = (gltf: any) => {
+      GltfPipelineLegacy.process(gltf);
+      return gltf;
+    };
+    options.customStages.push(customStage);
+    const result = await GltfPipeline.processGlb(glbBuffer, options);
+    return result.glb;
   }
-
-  private static fixBatchIdSemantic(gltf: any) {
-    const meshes = gltf.meshes;
-    for (const meshId in meshes) {
-      if (Object.prototype.hasOwnProperty.call(meshes, meshId)) {
-        const primitives = meshes[meshId].primitives;
-        const primitivesLength = primitives.length;
-        for (let i = 0; i < primitivesLength; ++i) {
-          const attributes = primitives[i].attributes;
-          if (attributes.BATCHID) {
-            attributes._BATCHID = attributes.BATCHID;
-            delete attributes.BATCHID;
-          }
-        }
-      }
-    }
-
-    const techniques = gltf.techniques;
-    for (const techniqueId in techniques) {
-      if (Object.prototype.hasOwnProperty.call(techniques, techniqueId)) {
-        const parameters = techniques[techniqueId].parameters;
-        for (const parameterId in parameters) {
-          if (Object.prototype.hasOwnProperty.call(parameters, parameterId)) {
-            const parameter = parameters[parameterId];
-            if (parameter.semantic === "BATCHID") {
-              parameter.semantic = "_BATCHID";
-            }
-          }
-        }
-      }
-    }
-  }
-  //==========================================================================
 }
