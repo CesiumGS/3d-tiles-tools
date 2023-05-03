fb09933c08c4153c77e1309c8d856da49b6d3280
Marco Hutter <javagl@javagl.de>
2023-05-03 19:09:11 +0200

    Add generated classes for tile formats and style

---
+++ src/structure/TileFormats/FeatureTable.ts
@@ -0,0 +1,8 @@
+import { RootProperty } from "../RootProperty";
+
+/**
+ * A set of semantics containing per-tile and per-feature values defining
+ * the position and appearance properties for features in a tile.
+ * @internal
+ */
+export interface FeatureTable extends RootProperty {}
---
+++ src/structure/TileFormats/I3dmFeatureTable.ts
@@ -0,0 +1,107 @@
+import { FeatureTable } from "./FeatureTable";
+import { BinaryBodyReference } from "./BinaryBodyReference";
+
+/**
+ * A set of Instanced 3D Model semantics that contains values defining
+ * the position and appearance properties for instanced models in a tile.
+ * @internal
+ */
+export interface I3dmFeatureTable extends FeatureTable {
+  /**
+   * A `BinaryBodyReference` object defining the reference to a section of
+   * the binary body where the property values are stored. Details about
+   * this property are described in the 3D Tiles specification.
+   */
+  POSITION?: BinaryBodyReference;
+
+  /**
+   * A `BinaryBodyReference` object defining the reference to a section of
+   * the binary body where the property values are stored. Details about
+   * this property are described in the 3D Tiles specification.
+   */
+  POSITION_QUANTIZED?: BinaryBodyReference;
+
+  /**
+   * A `BinaryBodyReference` object defining the reference to a section of
+   * the binary body where the property values are stored. Details about
+   * this property are described in the 3D Tiles specification.
+   */
+  NORMAL_UP?: BinaryBodyReference;
+
+  /**
+   * A `BinaryBodyReference` object defining the reference to a section of
+   * the binary body where the property values are stored. Details about
+   * this property are described in the 3D Tiles specification.
+   */
+  NORMAL_RIGHT?: BinaryBodyReference;
+
+  /**
+   * A `BinaryBodyReference` object defining the reference to a section of
+   * the binary body where the property values are stored. Details about
+   * this property are described in the 3D Tiles specification.
+   */
+  NORMAL_UP_OCT32P?: BinaryBodyReference;
+
+  /**
+   * A `BinaryBodyReference` object defining the reference to a section of
+   * the binary body where the property values are stored. Details about
+   * this property are described in the 3D Tiles specification.
+   */
+  NORMAL_RIGHT_OCT32P?: BinaryBodyReference;
+
+  /**
+   * A `BinaryBodyReference` object defining the reference to a section of
+   * the binary body where the property values are stored. Details about
+   * this property are described in the 3D Tiles specification.
+   */
+  SCALE?: BinaryBodyReference;
+
+  /**
+   * A `BinaryBodyReference` object defining the reference to a section of
+   * the binary body where the property values are stored. Details about
+   * this property are described in the 3D Tiles specification.
+   */
+  SCALE_NON_UNIFORM?: BinaryBodyReference;
+
+  /**
+   * A `BinaryBodyReference` object defining the reference to a section of
+   * the binary body where the property values are stored. Details about
+   * this property are described in the 3D Tiles specification.
+   */
+  BATCH_ID?: BinaryBodyReference;
+
+  /**
+   * A `GlobalPropertyInteger` object defining an integer property for all
+   * features. Details about this property are described in the 3D Tiles
+   * specification.
+   */
+  INSTANCES_LENGTH: number;
+
+  /**
+   * A `GlobalPropertyCartesian3` object defining a 3-component numeric
+   * property for all features. Details about this property are described
+   * in the 3D Tiles specification.
+   */
+  RTC_CENTER?: BinaryBodyOffset | number[];
+
+  /**
+   * A `GlobalPropertyCartesian3` object defining a 3-component numeric
+   * property for all features. Details about this property are described
+   * in the 3D Tiles specification.
+   */
+  QUANTIZED_VOLUME_OFFSET?: BinaryBodyOffset | number[];
+
+  /**
+   * A `GlobalPropertyCartesian3` object defining a 3-component numeric
+   * property for all features. Details about this property are described
+   * in the 3D Tiles specification.
+   */
+  QUANTIZED_VOLUME_SCALE?: BinaryBodyOffset | number[];
+
+  /**
+   * A `GlobalPropertyBoolean` object defining a boolean property for all
+   * features. Details about this property are described in the 3D Tiles
+   * specification.
+   */
+  EAST_NORTH_UP?: boolean;
+}
