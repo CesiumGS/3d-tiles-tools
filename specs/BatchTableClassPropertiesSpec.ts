551fbea2dd28d72846c61485a5e283f0704369dc
Marco Hutter <javagl@javagl.de>
2023-06-12 01:18:24 +0200

    Fix build error from spec.

--- specs/BatchTableClassPropertiesSpec.ts
+++ specs/BatchTableClassPropertiesSpec.ts
@@ -2,16 +2,13 @@ import { readJsonUnchecked } from "./metadata/readJsonUnchecked";
 import { BatchTableClassProperties } from "../src/migration/BatchTableClassProperties";
 
 describe("BatchTableClassProperties", function () {
-  beforeEach(async function () {
+  it("obtains the right type for example_STRING", async function () {
+    const propertyName = "example_STRING";
     const tileset = await readJsonUnchecked(
       "specs/data/TilesetWithFullMetadata/tileset.json"
     );
-    this.metadata = tileset.metadata;
-  });
-
-  it("obtains the right type for example_STRING", function () {
-    const propertyName = "example_STRING";
-    const value = this.metadata[propertyName];
+    const metadata = tileset.metadata;
+    const value = metadata[propertyName];
 
     const classProperty = BatchTableClassProperties.createClassProperty(
       propertyName,
