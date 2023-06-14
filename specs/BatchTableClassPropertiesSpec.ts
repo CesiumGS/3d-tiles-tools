a0207a971cf46dfe2e921dadaca922983b0f499f
Marco Hutter <javagl@javagl.de>
2023-06-15 01:32:15 +0200

    Fix in preliminary test stub

--- specs/BatchTableClassPropertiesSpec.ts
+++ specs/BatchTableClassPropertiesSpec.ts
@@ -1,14 +1,18 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { readJsonUnchecked } from "./metadata/readJsonUnchecked";
+
+import { Tileset } from "../src/structure/Tileset";
+
 import { BatchTableClassProperties } from "../src/migration/BatchTableClassProperties";
 
 describe("BatchTableClassProperties", function () {
-  it("obtains the right type for example_STRING", async function () {
-    const propertyName = "example_STRING";
-    const tileset = await readJsonUnchecked(
+  it("obtains the right type for example_fixed_length_STRING_array", async function () {
+    const propertyName = "example_fixed_length_STRING_array";
+    const tileset = (await readJsonUnchecked(
       "specs/data/TilesetWithFullMetadata/tileset.json"
-    );
-    const metadata = tileset.metadata;
-    const value = metadata[propertyName];
+    )) as Tileset;
+    const metadata = tileset.metadata!;
+    const value = metadata.properties![propertyName];
 
     const classProperty = BatchTableClassProperties.createClassProperty(
       propertyName,
