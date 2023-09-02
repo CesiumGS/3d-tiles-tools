01bdf3b75770253d952f1cd3f60440ddfdeb742a
Marco Hutter <javagl@javagl.de>
2023-09-02 15:52:52 +0200

    Prepare option to write logs to files

--- src/logging/LoggerFactory.ts
+++ src/logging/LoggerFactory.ts
@@ -1,65 +1,79 @@
 import pino from "pino";
+import { TransportTargetOptions } from "pino";
 import { Logger } from "pino";
 import { LoggerOptions } from "pino";
 
 /**
  * Internal function to create a pino Logger with default configuration.
  *
- * @param console - Whether log output should be pretty-printed
- * instead of generating newline-terminated JSON. Default: `true`
+ * @param console - Whether log output should be pretty-printed to the console
+ * (default: `true`)
+ * @param logFilePath - The path of the log file to write, or `undefined`
+ * to write no log file (default: `undefined`)
  * @returns The Logger
  */
-function createDefaultLogger(console?: boolean): Logger {
+function createDefaultLogger(console?: boolean, logFilePath?: string): Logger {
+  const consoleLogLevel = process.env.LOG_LEVEL || "trace";
+  const fileLogLevel = process.env.LOG_LEVEL || "trace";
+  const globalLogLevel = process.env.LOG_LEVEL || "trace";
+
+  const transportTargetOptionsList: TransportTargetOptions[] = [];
+
+  // The options for the 'console' transport target
+  if (console !== false) {
+    const consoleTargetOptions = {
+      level: consoleLogLevel,
+      target: "pino-pretty",
+      options: {},
+    };
+    transportTargetOptionsList.push(consoleTargetOptions);
+  }
+
+  // The options for the log file transport target
+  if (logFilePath !== undefined) {
+    const fileTargetOptions = {
+      level: fileLogLevel,
+      target: "pino/file",
+      options: {
+        destination: logFilePath,
+        append: false,
+      },
+    };
+    transportTargetOptionsList.push(fileTargetOptions);
+  }
+
+  // Create the actual transports based on the given options
+  const transport = pino.transport({
+    targets: transportTargetOptionsList,
+  });
+
   // Common logger options
-  const commonLoggerOptions: LoggerOptions = {
-    level: process.env.LOG_LEVEL || "trace",
+  const loggerOptions: LoggerOptions = {
+    level: globalLogLevel,
 
     // Avoid adding the PID and hostname to each log
     base: undefined,
-  };
-
-  // Options for the console logger
-  const consoleLoggerOptions: LoggerOptions = {
-    ...commonLoggerOptions,
-
-    // Enable pretty printing to the console by default
-    transport: {
-      target: "pino-pretty",
-    },
-  };
-
-  // Options for the JSON logger
-  const jsonLoggerOptions: LoggerOptions = {
-    ...commonLoggerOptions,
 
     // Format the time stamp as an ISO 8601-formatted time
     timestamp: pino.stdTimeFunctions.isoTime,
-
-    // Use the uppercase level name of the log level in JSON output
-    formatters: {
-      level(level: string) {
-        return { level: level.toUpperCase() };
-      },
-    },
   };
 
-  const logger = pino(
-    console === false ? jsonLoggerOptions : consoleLoggerOptions
-  );
+  const logger = pino(loggerOptions, transport);
   return logger;
 }
 
-const defaultLogger = createDefaultLogger();
+const defaultLogger = createDefaultLogger(true, undefined);
 
 /**
- * Creates a logger for the specified file.
+ * Creates a logger with the specified name.
  *
- * If the `fileName` is undefined, then a default logger will be
+ * If the `loggerName` is undefined, then a default logger will be
  * returned.
  *
- * Otherwise, the base name of the given file name (i.e. the part
- * after the last `/` or `\\`) will be included as a `name` in the
- * log messages.
+ * Otherwise, the name will be included as a `name` in the
+ * log messages. If the given name is a path (i.e. if it
+ * contains `/` or `\\`), then the part after the last `/`
+ * or `\\` will be used as the name.
  *
  * Example:
  * ```
@@ -67,19 +81,19 @@ const defaultLogger = createDefaultLogger();
  * const logger = LoggerFactory(__filename);
  * ```
  *
- * @param fileName - An optional file name
+ * @param loggerName - An optional name
  * @returns The Logger
  */
-const createLogger = (fileName?: string): Logger => {
-  if (fileName === undefined) {
+const createLogger = (loggerName?: string): Logger => {
+  if (loggerName === undefined) {
     return defaultLogger;
   }
-  let name = fileName;
+  let name = loggerName;
   let index = -1;
-  index = fileName.lastIndexOf("/");
-  index = Math.max(index, fileName.lastIndexOf("\\"));
+  index = loggerName.lastIndexOf("/");
+  index = Math.max(index, loggerName.lastIndexOf("\\"));
   if (index !== -1) {
-    name = fileName.substring(index + 1);
+    name = loggerName.substring(index + 1);
   }
   return defaultLogger.child({ name: name });
 };
