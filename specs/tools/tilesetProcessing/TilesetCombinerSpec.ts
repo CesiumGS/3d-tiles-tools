c0fcbb5a0d6bfdff3b67005c9e713c05da1396e6
Marco Hutter <javagl@javagl.de>
2024-04-07 16:07:03 +0200

    Add spec for combining with transforms in externals

--- specs/tools/tilesetProcessing/TilesetCombinerSpec.ts
+++ specs/tools/tilesetProcessing/TilesetCombinerSpec.ts
@@ -8,10 +8,16 @@ import { SpecHelpers } from "../../SpecHelpers";
 
 const SPECS_DATA_BASE_DIRECTORY = SpecHelpers.getSpecsDataBaseDirectory();
 
-const basicInput =
+const nestedExteralInput =
   SPECS_DATA_BASE_DIRECTORY + "/combineTilesets/nestedExternal";
-const basicOutput =
+const nestedExteralOutput =
   SPECS_DATA_BASE_DIRECTORY + "/output/combineTilesets/nestedExternal";
+
+const externalsWithTransformInput =
+  SPECS_DATA_BASE_DIRECTORY + "/combineTilesets/externalsWithTransform";
+const externalsWithTransformOutput =
+  SPECS_DATA_BASE_DIRECTORY + "/output/combineTilesets/externalsWithTransform";
+
 const overwrite = true;
 
 describe("TilesetCombiner", function () {
@@ -22,12 +28,16 @@ describe("TilesetCombiner", function () {
   });
 
   it("combines external tilesets into a single tileset", async function () {
-    await TilesetOperations.combine(basicInput, basicOutput, overwrite);
+    await TilesetOperations.combine(
+      nestedExteralInput,
+      nestedExteralOutput,
+      overwrite
+    );
 
     // Ensure that the output directory contains the expected files:
     // All files of the input, except for the external tileset JSON files
     const actualRelativeFiles =
-      SpecHelpers.collectRelativeFileNames(basicOutput);
+      SpecHelpers.collectRelativeFileNames(nestedExteralOutput);
     actualRelativeFiles.sort();
     const expectedRelativeFiles = [
       "README.md",
@@ -44,7 +54,7 @@ describe("TilesetCombiner", function () {
     // Ensure that the single 'tileset.json' contains the
     // proper content URIs for the combined output
     const tilesetJsonBuffer = fs.readFileSync(
-      Paths.join(basicOutput, "tileset.json")
+      Paths.join(nestedExteralOutput, "tileset.json")
     );
     const tileset = JSON.parse(tilesetJsonBuffer.toString());
     const actualContentUris = await SpecHelpers.collectExplicitContentUris(
@@ -61,5 +71,55 @@ describe("TilesetCombiner", function () {
       "tileB.b3dm",
     ];
     expect(actualContentUris).toEqual(expectedContentUris);
+  });
+
+  it("retains the transforms of root nodes of external tilesets", async function () {
+    await TilesetOperations.combine(
+      externalsWithTransformInput,
+      externalsWithTransformOutput,
+      overwrite
+    );
+
+    // Ensure that the resulting tileset JSON contains the
+    // proper content transforms and bounding volumes
+    const tilesetJsonBuffer = fs.readFileSync(
+      Paths.join(externalsWithTransformOutput, "tileset.json")
+    );
+    const tileset = JSON.parse(tilesetJsonBuffer.toString());
+
+    // The expected bounding box for the root is
+    // the bounding box of (0,0,0)-(4,5,6)
+    const expectedRootBox = [2, 1.5, 3, 2, 0, 0, 0, -2.5, 0, 0, 0, 3];
+    const rootBox = tileset.root.boundingVolume.box;
+    expect(rootBox).toEqual(expectedRootBox);
+
+    // The expected bounding box for each child is the unit
+    // cube (it will be transformed into place by the
+    // transform of the children!
+    const expectedChildBox = [0.5, -0.5, 0.5, 0.5, 0, 0, 0, -0.5, 0, 0, 0, 0.5];
+
+    const childBox0 = tileset.root.children[0].boundingVolume.box;
+    const childBox1 = tileset.root.children[1].boundingVolume.box;
+    const childBox2 = tileset.root.children[2].boundingVolume.box;
+    expect(childBox0).toEqual(expectedChildBox);
+    expect(childBox1).toEqual(expectedChildBox);
+    expect(childBox2).toEqual(expectedChildBox);
+
+    // The transforms of the nodes of the children that are created for the
+    // external tilesets are expected to be their original transforms, namely,
+    // - a translation of x=3 for the red cube
+    // - a translation of y=4 for the green cube
+    // - a translation of z=5 for the blue cube
+    const expectedTransformR = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 3, 0, 0, 1];
+    const childTransformR = tileset.root.children[0].transform;
+    expect(childTransformR).toEqual(expectedTransformR);
+
+    const expectedTransformG = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 4, 0, 1];
+    const childTransformG = tileset.root.children[1].transform;
+    expect(childTransformG).toEqual(expectedTransformG);
+
+    const expectedTransformB = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 5, 1];
+    const childTransformB = tileset.root.children[2].transform;
+    expect(childTransformB).toEqual(expectedTransformB);
   });
 });
