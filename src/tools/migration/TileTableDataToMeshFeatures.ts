e05547b2b14f9e268125aea916b58fbb5fff8488
Marco Hutter <javagl@javagl.de>
2024-05-05 17:09:22 +0200

    Avoid duplicate storage of batch- and feature IDs

--- src/tools/migration/TileTableDataToMeshFeatures.ts
+++ src/tools/migration/TileTableDataToMeshFeatures.ts
@@ -18,21 +18,30 @@ const logger = Loggers.get("migration");
  */
 export class TileTableDataToMeshFeatures {
   /**
-   * Convert the `_BATCHID` attribute in the given primitive into
-   * an instance of the `ETX_mesh_features` extension that is
-   * associated with this primitive, storing the former `_BATCHID`
-   * attribute as a new `_FEATURE_ID_0` attribute.
+   * Convert the `_BATCHID` or `BATCHID` attribute in the given
+   * primitive into an instance of the `ETX_mesh_features`
+   * extension that is associated with this primitive, storing
+   * the former batch ID attribute as a new `_FEATURE_ID_0` attribute.
+   *
+   * Note that this will remove the former batch ID attributes
+   * in the given primitive to `null`, but it will not dispose
+   * the corresponding accessors. These have to be disposed
+   * after all primitives have been processed.
    *
    * @param document - The glTF-Transform document
    * @param primitive - The glTF-Transform primitive
+   * @param batchIdToFeatureIdAccessor - A mapping from former
+   * batch ID accessors to feature ID acccessors, to keep
+   * track of the accessors that have already been converted
    * @returns The glTF-Transform `FeatureId` object that has
    * been created
    * @throws TileFormatError If the given primitive does not
-   * contain a valid `_BATCHID` attribute.
+   * contain a valid `_BATCHID` or `BATCHID` attribute.
    */
   static convertBatchIdToMeshFeatures(
     document: Document,
-    primitive: Primitive
+    primitive: Primitive,
+    batchIdToFeatureIdAccessor: Map<Accessor, Accessor>
   ): FeatureId {
     let batchIdAttribute = primitive.getAttribute("_BATCHID");
     if (!batchIdAttribute) {
@@ -48,6 +57,7 @@ export class TileTableDataToMeshFeatures {
         );
       }
     }
+
     const batchIdsArray = batchIdAttribute.getArray();
     if (!batchIdsArray) {
       throw new TileFormatError(
@@ -55,22 +65,31 @@ export class TileTableDataToMeshFeatures {
       );
     }
 
-    const extMeshFeatures = document.createExtension(EXTMeshFeatures);
+    // Re-use any existing feature ID accessor that may already
+    // have been created for the batch ID accessor
+    let featureIdAccessor = batchIdToFeatureIdAccessor.get(batchIdAttribute);
+    if (featureIdAccessor === undefined) {
+      // Create a new accessor for the _FEATURE_ID_0,
+      // containing the values of the _BATCHID accessor
+      featureIdAccessor = document.createAccessor();
+      const buffer = document.getRoot().listBuffers()[0];
+      featureIdAccessor.setBuffer(buffer);
+      featureIdAccessor.setType(Accessor.Type.SCALAR);
+      featureIdAccessor.setArray(batchIdsArray);
 
-    // Assign a new accessor for the _FEATURE_ID_0 to the primitive,
-    // containing the values of the _BATCHID accessor
-    const featureIdAccessor = document.createAccessor();
-    const buffer = document.getRoot().listBuffers()[0];
-    featureIdAccessor.setBuffer(buffer);
-    featureIdAccessor.setType(Accessor.Type.SCALAR);
-    featureIdAccessor.setArray(batchIdsArray);
+      batchIdToFeatureIdAccessor.set(batchIdAttribute, featureIdAccessor);
+    }
+
+    // Assign the feature ID accessor to the primitive
     primitive.setAttribute("_FEATURE_ID_0", featureIdAccessor);
 
-    // Remove the former _BATCHID attribute
+    // Remove the former _BATCHID OR BATCHID attribute
     primitive.setAttribute("_BATCHID", null);
+    primitive.setAttribute("BATCHID", null);
 
     // Creates the mesh features extension object that
     // refers to the _FEATURE_ID_0 attribute
+    const extMeshFeatures = document.createExtension(EXTMeshFeatures);
     const meshFeatures = extMeshFeatures.createMeshFeatures();
     const featureId = extMeshFeatures.createFeatureId();
     featureId.setAttribute(0);
@@ -79,6 +98,7 @@ export class TileTableDataToMeshFeatures {
     meshFeatures.addFeatureId(featureId);
 
     primitive.setExtension("EXT_mesh_features", meshFeatures);
+
     return featureId;
   }
 }
